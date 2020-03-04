848c9879760a8f4e2b9601056bd575afffd66294
sjx-git <1064200847@qq.com>
2020-03-04 21:26:57 +0800

    fix

--- APi_Test_Package/Auto_TestSuite.py
+++ APi_Test_Package/Auto_TestSuite.py
@@ -1,8 +1,3 @@
-import sys
-import os
-curPath = os.path.abspath(os.path.dirname(__file__))
-rootPath = os.path.split(curPath)[0]
-sys.path.append(rootPath)
 import unittest
 from APi_Test_Package import Request_get,Request_post
 class Test_Suite(unittest.TestCase):
