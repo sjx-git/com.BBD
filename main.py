9b80c13b524ebfa788f8aad75132c24af5ee4327
sjx-git <1064200847@qq.com>
2020-03-04 20:01:01 +0800

    'try'

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 from APi_Test_Package import Request_post,Request_get,Auto_TestReport,Auto_TestSuite,Auto_TestUnitest
+import sys
+sys.path.append('/root/test/com.bbd')
+sys.path.append('/root/test/com.bbd/APi_Test_Package')
+print(sys.path)
