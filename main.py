c6445277cb47c64e4da12a312befcf9250d1f446
sjx-git <1064200847@qq.com>
2020-03-05 21:59:08 +0800

    fix

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from HTMLTestRunner import HTMLTestRunner #遇到两次导入只导入了文件�
 if __name__ == '__main__':
     #unittest.main(Auto_TestUnitest)
 
-    #suite = unittest.TestSuite()
+    suite = unittest.TestSuite()
     #加载用例的时候，一定注意，必须加载的是 加载 类的 类名，不可以直接写到文件名就结束了
-    #suite.addTests(unittest.TestLoader().loadTestsFromTestCase(Auto_TestUnitest.Auto_test))
-    #unittest.TextTestRunner(verbosity=2).run(suite)
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(Auto_TestUnitest.Auto_test))
+    unittest.TextTestRunner(verbosity=2).run(suite)
     
-    #unittest.TextTestRunner().run(Auto_TestSuite.Test_Suite().suite_demo())
+    unittest.TextTestRunner().run(Auto_TestSuite.Test_Suite().suite_demo())
     with open("TestReport11.html","wb") as f:
         runner = HTMLTestRunner(stream = f,title = u'测试报告',description= u'测试情况',verbosity =2)
         runner.STYLESHEET_TMPL = '<link rel="stylesheet" href="my_stylesheet.css" type="text/css">'
