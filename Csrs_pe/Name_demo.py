bda7a7b363b240aa23a604e5c7afef66c5aa9106
sjx-git <1064200847@qq.com>
2020-03-24 22:13:07 +0800

    'ok'

--- Csrs_pe/Name_demo.py
+++ Csrs_pe/Name_demo.py
@@ -3,8 +3,12 @@ import xlrd
 class New_name(object):
     def my_name(self):
         book = xlrd.open_workbook("../data/Csrc_pe.xlsx")
-        xl = book.sheet_by_index('Sheet1')
-        print(xl.nrows)
+        xl = book.sheet_by_name('Sheet2')
+        #print(xl.nrows)
+        col_num = xl.col_values(3)[1:]
+        #print(col_num)
+        return col_num
+        #print(len(xl.col_values(3)[1:]))
 
 if __name__ == '__main__':
     New_name().my_name()
