b355446a1754dbae3a7e95142f9d7d67e081008d
sjx-git <1064200847@qq.com>
2020-03-25 11:31:45 +0800

    'ok'

--- Csrs_pe/Name_demo.py
+++ Csrs_pe/Name_demo.py
@@ -3,11 +3,20 @@ import xlrd
 class New_name(object):
     def my_name(self):
         book = xlrd.open_workbook("../data/Csrc_pe.xlsx")
-        xl = book.sheet_by_name('Sheet2')
+        xl = book.sheet_by_name('Sheet1')
         #print(xl.nrows)
-        col_num = xl.col_values(3)[1:]
-        #print(col_num)
-        return col_num
+        post_name = xl.col_values(3)[1:]
+        #print(post_name)
+        get_name =xl.col_values(5)[1:]
+        #print(get_name)
+        #print(len(get_name))
+        '''
+          for temp,tem in zip(post_name,get_name):
+            par = {"account":"admin","deptName":"default",
+                              "feedbackIndex":tem,"content":temp,"operation":0}
+            print(par)
+        '''
+        return post_name,get_name
         #print(len(xl.col_values(3)[1:]))
 
 if __name__ == '__main__':
--- Csrs_pe/new_target.py
+++ Csrs_pe/new_target.py
@@ -9,16 +9,20 @@ class target(object):
     header = Headers_demo.headers_demo().New_headers()
     #第三部设置必要的参数
     time2 = Time_demo.New_datetime().my_time()
-    col = Name_demo.New_name().my_name()
+    post_name,get_name = Name_demo.New_name().my_name()
 
     def Target_new(self):
-        for temp in self.col:
+        '''
+        zip() 函数用于将可迭代的对象作为参数，将对象中对应的元素打包成一个个元组，然后返回由这些元组组成的列表。
+        如果各个迭代器的元素个数不一致，则返回列表长度与最短的对象相同
+        '''
+        for temp,tem in zip(self.post_name,self.get_name):
             par = json.dumps({"account":"admin","deptName":"default","updateTime":self.time2,
-                              "feedbackIndex":"naturalPerson","content":temp,"operation":0})
+                              "feedbackIndex":tem,"content":temp,"operation":0})
             res = requests.post(self.url1,headers = self.header,data=par)
             #print(res.text)
             #print(res.json())
-            print('后端指标---%s---的反馈内容是:---%s---,反馈结果是:---%s---'%("naturalPerson",temp,res.json()['data']))
+            print('后端指标---%s---的反馈内容是:---%s---,反馈结果是:---%s---'%(tem,temp,res.json()['data']))
 
 if __name__ == '__main__':
     target().Target_new()
