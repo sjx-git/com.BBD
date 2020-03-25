3c163b704d23b17ad2e0abd36057f99872b5b074
sjx-git <1064200847@qq.com>
2020-03-25 20:21:41 +0800

    'fix'

--- Csrs_pe/old_target.py
+++ Csrs_pe/old_target.py
@@ -1,20 +1,23 @@
 from Csrs_pe  import Headers_demo,Oldname_demo
-import  requests,json
+import  requests,json,random
 class old_name(object):
     head = Headers_demo.headers_demo().New_headers()
     old_post_name,old_uid = Oldname_demo.old_name().My_oldname()
     url1 = 'http://10.28.200.165/csrc_pe/api/v1.0/risk/investigation/index/self/check'
-    for temp in range(old_uid):
+    #range只能跟范围，列表就需要用random.choices
+    for temp in random.choices(old_uid):
+        print(temp)
         data1 = json.dumps({
             "account": "admin",
             "deptName": "default",
             "allIndex": "false",
             "indexNames": old_post_name,
-            "pofMngmntPsnIds": temp
+            "pofMngmntPsnIds": [temp]
         }
     )
     def My_oldName(self):
         res = requests.post(self.url1,headers = self.head,data=self.data1)
+        #print(res.json())
         print('发送结果是：%s'%(res.json()["message"]))
 
 if __name__ == '__main__':
