cc2119ff5f25e4ff951dbf2850bc16c0f9aa58af
sjx-git <1064200847@qq.com>
2020-03-26 15:16:56 +0800

    'fix'

--- Csrs_pe/Oldname_demo.py
+++ Csrs_pe/Oldname_demo.py
@@ -1,14 +1,27 @@
-import  xlrd
+import  xlrd,random
 
 class old_name(object):
     def My_oldname(self):
         book = xlrd.open_workbook("../data/Csrc_pe.xlsx")
         xl = book.sheet_by_name('可发送指标')
+        '''
+        #此类方法，是用获取列的方式，得到想要得id，但是不能同时获取到id和name，所以用下面的方法
         #print(xl.nrows)
-        old_post_name = xl.col_values(0)[1:]
-        old_id = xl.col_values(3)[1:]
+        old_post_name = xl.col_values(0)[1:]#指标列表
+        old_id = xl.col_values(3)[1:]#管理人id
+        #old_name = xl.col_values(4)[1:]#管理人名称
         #print(old_post_name)
         return old_post_name,old_id
-
+        '''
+        old_post_name = xl.col_values(0)[1:]#指标列表
+        s = random.randint(0,xl.nrows)#用ranint，获取一个数；random.randrange(1,10,2))   #随机一个大于等于1且小于等于10之间的奇数，其中2表示递增基数
+        #print(s)
+        #print(xl.row_values(s))
+        old_post_name = xl.col_values(0)[1:32]#指标列表
+        old_id = xl.row_values(s)[3]#管理人id
+        old_name1 =xl.row_values(s)[4]#管理人名称
+        #print(type(list(old_id)))
+        #print(old_post_name,old_id,old_name1)
+        return old_post_name,old_name1,old_id
 if __name__ == '__main__':
     old_name().My_oldname()
--- Csrs_pe/old_target.py
+++ Csrs_pe/old_target.py
@@ -1,24 +1,28 @@
 from Csrs_pe  import Headers_demo,Oldname_demo
 import  requests,json,random
 class old_name(object):
-    head = Headers_demo.headers_demo().New_headers()
-    old_post_name,old_uid = Oldname_demo.old_name().My_oldname()
-    url1 = 'http://10.28.200.165/csrc_pe/api/v1.0/risk/investigation/index/self/check'
-    #range只能跟范围，列表就需要用random.choices
+    '''
+    #此类方法，是用for循环的方式 通过以列为单位，获取到id，但是没法同时获取到name，所以用下面的方法
+    #range只能跟范围，列表就需要用random.choices；random.sample(list，2)  #随机返回参数列表中任意两个元素，参数二指定返回的数量
     for temp in random.choices(old_uid):
-        print(temp)
-        data1 = json.dumps({
-            "account": "admin",
-            "deptName": "default",
-            "allIndex": "false",
-            "indexNames": old_post_name,
-            "pofMngmntPsnIds": [temp]
-        }
-    )
+        #print(temp)
+        data1 = json.dumps({   "account": "admin","deptName": "default","allIndex": "false","indexNames": old_post_name,
+            "pofMngmntPsnIds": [temp]   }
+                )
+        '''
+    head = Headers_demo.headers_demo().New_headers()
+    old_post_name,old_name1,old_id = Oldname_demo.old_name().My_oldname()
+    url1 = 'http://10.28.200.165/csrc_pe/api/v1.0/risk/investigation/index/self/check'
+    data1 = json.dumps({
+        "account": "admin", "deptName": "default","allIndex": "false","indexNames": old_post_name,
+        "pofMngmntPsnIds": [old_id]}
+     )#这里的参数需要的是列表，转一下
+
     def My_oldName(self):
         res = requests.post(self.url1,headers = self.head,data=self.data1)
+        #print(self.data1)
         #print(res.json())
-        print('发送结果是：%s'%(res.json()["message"]))
+        print('发送的指标的个数是：%s，管理人id是：---%s---,管理人名称是：%s，结果是：---%s---'%(len(self.old_post_name),self.old_id,self.old_name1,res.json()["message"]))
 
 if __name__ == '__main__':
     old_name().My_oldName()
