658100b45d718e241c23713bf38f9a58f6999be9
sjx-git <1064200847@qq.com>
2020-03-26 08:31:50 +0800

    'fix'

--- Csrs_pe/Oldname_demo.py
+++ Csrs_pe/Oldname_demo.py
@@ -3,7 +3,7 @@ import  xlrd
 class old_name(object):
     def My_oldname(self):
         book = xlrd.open_workbook("../data/Csrc_pe.xlsx")
-        xl = book.sheet_by_name('Sheet2')
+        xl = book.sheet_by_name('可发送指标')
         #print(xl.nrows)
         old_post_name = xl.col_values(0)[1:]
         old_id = xl.col_values(3)[1:]
